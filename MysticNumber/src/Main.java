1df11be8edc1393a729f09e2bb50bc1c32ab3b03
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-07-14 17:38:44 +0300

    Some weird statistics

--- MysticNumber/src/Main.java
+++ MysticNumber/src/Main.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) {
-        MysticNumber secret = new MysticNumber(new Integer[]{9, 2, 3, 8});
-        System.out.println("Secret: " + secret);
-
+    static int solve(MysticNumber secret) {
         RangeTable table = new RangeTable();
         for (MysticNumber number: Generator.getAll())
             table.addNumber(number);
@@ -18,7 +16,7 @@ public class Main {
         Range range = new Range(secret, sample);
 
         while (true) {
-            System.out.println("Iteration " + iteration + ": sample " + sample + ", range " + range);
+            //System.out.println("Iteration " + iteration + ": sample " + sample + ", range " + range);
             iteration++;
 
             tables.add(new RangeTable());
@@ -33,15 +31,33 @@ public class Main {
                 sample = tables.get(iteration).getNumbers().get(0);
             }
             catch (IndexOutOfBoundsException e) {
-                break;
+                return iteration;
             }
 
             if (tables.get(iteration).getNumbers().isEmpty()) {
-                System.out.println(sample);
-                break;
+                return iteration;
             }
             tables.get(iteration).getNumbers().remove(0);
             range = new Range(secret, sample);
         }
+    }
+
+    static void checkIterations() {
+        HashMap<Integer, Integer> stat = new HashMap<>();
+        for (MysticNumber number: Generator.getAll()) {
+            int iterations = solve(number);
+            stat.merge(iterations, 1, Integer::sum);
+        }
+
+        System.out.println("*** Statistics ***");
+        for (int key: stat.keySet()) {
+            System.out.println(key + " iterations: " + stat.get(key) + " cases");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkIterations();
+
+        System.out.println(solve(new MysticNumber(new Integer[]{1, 2, 3, 4})));
     }
 }
